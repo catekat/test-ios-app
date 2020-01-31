29d6a871edc022e71dbc85a637fd76f92baa0c69
Xiaoyan Yang <xiaoyanyang@Xiaoyans-MacBook-Pro.local>
2020-01-31 02:53:57 -0500

    Test

--- TestiOSApp/ViewController.swift
+++ TestiOSApp/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var helloWorldLabel: UILabel!
+    @IBOutlet weak var doStuffButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // this gets executed as soon as the View Controller appears on the phone
     }
-
+    
+    
+    @IBAction func doStuff(_ sender: Any) {
+        // this gets executed when we click the button
+        helloWorldLabel.text = "Caitlyn Chen"
+    }
+    
 
 }
 
